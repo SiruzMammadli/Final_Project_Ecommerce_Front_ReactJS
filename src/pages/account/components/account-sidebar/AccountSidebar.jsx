6e97f97b3191d19b06f8dfba5f46c3a1ca2609a4
SiruzMammadli <siruz.mammadli@gmail.com>
2023-03-13 16:07:55 +0400

    updated_incompleted_account_page_1

--- src/pages/account/components/account-sidebar/AccountSidebar.jsx
+++ src/pages/account/components/account-sidebar/AccountSidebar.jsx
@@ -1,5 +1,35 @@
 import "./styles/account-sidebar.scss";
 
 export default function AccountSidebar() {
-  return <aside className="account_sidebar p-inline-15">Sidebar</aside>;
+  return (
+    <aside className="account_sidebar p-inline-15">
+      <ul className="nav_tab">
+        <li className="tab_item active">
+          <a href="#">
+            <i className="bx bxs-dashboard icon"></i> Kontrol paneli
+          </a>
+        </li>
+        <li className="tab_item">
+          <a href="#">
+            <i className="bx bx-basket icon"></i> Sifarişlər
+          </a>
+        </li>
+        <li className="tab_item">
+          <a href="#">
+            <i className="bx bx-home icon"></i> Ünvanlar
+          </a>
+        </li>
+        <li className="tab_item">
+          <a href="#">
+            <i className="bx bx-user icon"></i> Məxfilik
+          </a>
+        </li>
+        <li className="tab_item">
+          <a href="#">
+            <i className="bx bx-log-out icon"></i> Hesabdan çıx
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
 }
